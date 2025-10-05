27b889dad243dc5bce45003b7db9e1794f31604b
Ansh-Ghawri <ghawri.ansh@gmail.com>
2025-10-05 23:27:33 +0530

    fix: env variables

--- gemini_openai.py
+++ gemini_openai.py
@@ -1,7 +1,12 @@
+import os
 from openai import OpenAI
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
 
 client = OpenAI(
-    api_key="AIzaSyAkaCySUabK-Fw8y5xEiCUSm2fMFF7Gv6M",
+    api_key=os.getenv("OPENAI_API_KEY"),
     base_url="https://generativelanguage.googleapis.com/v1beta/"
 )
 
